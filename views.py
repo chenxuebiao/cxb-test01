466cfce84f6ce2b6769585cc879b7a6087e64e61
chenxuebiao <1162413505@qq.com>
2020-05-27 22:30:53 +0800

    1

--- views.py
+++ views.py
@@ -1,1 +1,8 @@
-print("hello python")
+from django.http import HttpRespose
+from django.shortcuts import redirect
+
+def index(request):
+    return HttpResponse('infex')
+
+def login(request):
+    return redirect('/index')
